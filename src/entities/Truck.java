2541964c89f6fbefb48a048485d4568570f49028
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-06 23:57:45 -0300

    Upadte Truck

--- src/entities/Truck.java
+++ src/entities/Truck.java
@@ -1,5 +1,24 @@
 package entities;
 
 public class Truck extends Vehicle {
+	
+	private Integer axles;
+	
+	public Truck() {
+		super();
+	}
+
+	public Truck(String plate, Integer year, Integer axles) {
+		super(plate, year);
+		this.axles = axles;
+	}
+
+	public Integer getAxles() {
+		return axles;
+	}
+
+	public void setAxles(Integer axles) {
+		this.axles = axles;
+	}
 
 }
