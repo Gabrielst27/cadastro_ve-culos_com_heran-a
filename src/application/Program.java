d509c921a5e3fb9dc33d79d4bf54bfeb9fcd770f
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-07 00:24:57 -0300

    Criada instanciação do caminhão

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 import entities.Bus;
 import entities.Truck;
+import entities.Vehicle;
 
 public class Program {
 
@@ -30,6 +31,21 @@ public class Program {
 			System.out.print("Enter vehicle seats: ");
 			int seats = sc.nextInt();
 			sc.nextLine();
+			
+			Bus bus = new Bus(plate, year, seats);
+			busses.add(bus);
+		} else if(type.toUpperCase().equals("TRUCK")) {
+			System.out.print("Enter vehicle plate: ");
+			String plate = sc.nextLine();
+			System.out.print("Enter vehicle year: ");
+			int year = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Enter vehicle axles: ");
+			int axles = sc.nextInt();
+			sc.nextLine();
+			
+			Truck truck = new Truck(plate, year, axles);
+			trucks.add(truck);
 		}
 		
 		sc.close();
