7809af7842b380714ff0e123ebfa80a06a907fbc
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-07 00:38:51 -0300

    Criada estrutura de repetição em Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,34 +19,49 @@ public class Program {
 		List<Truck> trucks = new ArrayList<>();
 		List<Bus> busses = new ArrayList<>();
 		
-		System.out.print("Enter vehicle type (Bus or Truck): ");
-		String type = sc.nextLine();
+		char aux = 'y';
 		
-		if(type.toUpperCase().equals("BUS")) {
-			System.out.print("Enter vehicle plate: ");
-			String plate = sc.nextLine();
-			System.out.print("Enter vehicle year: ");
-			int year = sc.nextInt();
-			sc.nextLine();
-			System.out.print("Enter vehicle seats: ");
-			int seats = sc.nextInt();
-			sc.nextLine();
+		while (aux == 'y') {
+			System.out.print("Enter vehicle type (Bus or Truck): ");
+			String type = sc.nextLine();
 			
-			Bus bus = new Bus(plate, year, seats);
-			busses.add(bus);
-		} else if(type.toUpperCase().equals("TRUCK")) {
-			System.out.print("Enter vehicle plate: ");
-			String plate = sc.nextLine();
-			System.out.print("Enter vehicle year: ");
-			int year = sc.nextInt();
+			if(type.toUpperCase().equals("BUS")) {
+				System.out.print("Enter vehicle plate: ");
+				String plate = sc.nextLine();
+				System.out.print("Enter vehicle year: ");
+				int year = sc.nextInt();
+				sc.nextLine();
+				System.out.print("Enter vehicle seats: ");
+				int seats = sc.nextInt();
+				sc.nextLine();
+				
+				Bus bus = new Bus(plate, year, seats);
+				busses.add(bus);
+				
+				System.out.print("Continue? (y/n): ");
+				aux = sc.next().charAt(0);
+			} else if(type.toUpperCase().equals("TRUCK")) {
+				System.out.print("Enter vehicle plate: ");
+				String plate = sc.nextLine();
+				System.out.print("Enter vehicle year: ");
+				int year = sc.nextInt();
+				sc.nextLine();
+				System.out.print("Enter vehicle axles: ");
+				int axles = sc.nextInt();
+				sc.nextLine();
+				
+				Truck truck = new Truck(plate, year, axles);
+				trucks.add(truck);
+				
+				System.out.print("Continue? (y/n): ");
+				aux = sc.next().charAt(0);
+			} else {
+				System.out.printf("ERROR! Invalid vehicle!\nTry again? (y/n): ");
+				aux = sc.next().charAt(0);
+			}
 			sc.nextLine();
-			System.out.print("Enter vehicle axles: ");
-			int axles = sc.nextInt();
-			sc.nextLine();
-			
-			Truck truck = new Truck(plate, year, axles);
-			trucks.add(truck);
 		}
+		
 		
 		sc.close();
 
