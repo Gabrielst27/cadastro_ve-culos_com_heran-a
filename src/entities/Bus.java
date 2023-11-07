b7c8e1e9c05a5283e436a9ec6daee2239fda0baf
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-06 23:58:42 -0300

    Update Bus

--- src/entities/Bus.java
+++ src/entities/Bus.java
@@ -1,5 +1,26 @@
 package entities;
 
 public class Bus extends Vehicle {
+	
+	private Integer seats;
+	
+	public Bus() {
+		super();
+	}
+
+	public Bus(String plate, Integer year, Integer seats) {
+		super(plate, year);
+		this.seats = seats;
+	}
+
+	public Integer getSeats() {
+		return seats;
+	}
+
+	public void setSeats(Integer seats) {
+		this.seats = seats;
+	}
+	
+	
 
 }
