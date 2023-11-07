71d2ee4db60a04b946af0dd357f4261e0e1b6a14
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-06 23:55:01 -0300

    Update Vehicle

--- src/entities/Vehicle.java
+++ src/entities/Vehicle.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Vehicle {
+	
+	private String plate;
+	private Integer year;
+	
+	public Vehicle() {
+		
+	}
+	
+	public Vehicle(String plate, Integer year) {
+		this.plate = plate;
+		this.year = year;
+	}
+
+	public String getPlate() {
+		return plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
 
 }
